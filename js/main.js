b9ea6c9359441b21a8fa52f8239f586b850209cc
Unknown <daniel.juiche@gmail.com>
2018-02-17 16:23:35 -0800

    Basic clicking logic

--- js/main.js
+++ js/main.js
@@ -2,28 +2,47 @@ $(document).ready(function() {
 
 	// Initialized variables
 	var dirt = 0;
+	var wood = 0;
+	var stone = 0;
+	var metal = 0;
+	var students = 0;
+	
 
 	// Object for active skills
 
 	// Object to keep track of our passives
 
 
-	// Direct upgrades
-
+	// Upgrades
 
 	// Game loop
 
 
 	// Play area clicking
 	$("#play-area").click(function() {
-		dirt_add(10);
+		resource_change(10,1,1,1,1);
 		console.log(dirt);
+		console.log(students);
+		update_resources();
 	});
 
-	// Functions for adding dirt
-	function dirt_add(amount) {
-		dirt += amount;
+	// Functions for adding resources
+	function resource_change(dirt_change, wood_change, stone_change, metal_change, students_change) {
+		dirt += dirt_change;
+		wood += wood_change;
+		stone += stone_change;
+		metal += metal_change;
+		students += students_change;
 	}
 
-	//jusung
+	function update_resources() {
+		$(".dirt-amount").html("Dirt: " + dirt);
+		$(".wood-amount").html("Wood: " + wood);
+		$(".stone-amount").html("Stone: " + stone);
+		$(".metal-amount").html("Metal: " + metal);
+		$(".students-amount").html("Students: " + students);
+
+
+	}
+
 });
